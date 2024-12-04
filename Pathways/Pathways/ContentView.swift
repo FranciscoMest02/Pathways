ad8692a9369cf4af0e0128f0a7d1dd7c48ee51b0
Francisco Mestizo <panchitomestizo@gmail.com>
2024-12-04 16:26:07 +0100

    Initial commit

---
+++ Pathways/Pathways/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pathways
+//
+//  Created by Francisco Mestizo on 04/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pathways/Pathways/PathwaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathwaysApp.swift
+//  Pathways
+//
+//  Created by Francisco Mestizo on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PathwaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
